8d3b9e5bc284cc51a79dfe24e9d6c32db1af1c49
ceiachi <ceiarosendal@gmail.com>
2024-10-11 20:44:52 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,94 @@
+/**
+  DISCORD SERVER : https://discord.gg/d9ZpAh3UwN
+ * **********************************************
+ *   Code by yzichi
+ * **********************************************
+ */
+
+
+
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const client = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to ceia: http://localhost:${port}`);
+  console.log(`🔗 Powered By ceia`);
+});
+
+
+const statusMessages = ["status 1" "status 2"];
+
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+/**
+  DISCORD SERVER : https://discord.gg/d9ZpAh3UwN
+ * **********************************************
+ *   Code by yzichi
+ * **********************************************
+ */
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'twitch stream',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
+
+client.once('ready', () => {
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
+});
+
+login();
+
+/**
+  DISCORD SERVER : https://discord.gg/d9ZpAh3UwN
+ * **********************************************
+ *   Code by yzichi
+ * **********************************************
+ */
